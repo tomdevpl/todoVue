505e068ed71ea1d8fc2f9f8290de09a1bc4c0281
tomdevpl <tomdevpl@gmail.com>
2019-04-11 09:12:39 +0200

    Build for hosting

---
+++ dist/js/about.5b5d74dd.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},a=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("h1",[t._v("About")]),n("p",[t._v("This is the TodoList app v1.0.0")])])}],u=n("2877"),i={},o=Object(u["a"])(i,s,a,!1,null,null,null);e["default"]=o.exports}}]);
+//# sourceMappingURL=about.5b5d74dd.js.map
